161f395975aa0deedaf69eb0af095af46db61e3c
Sergey Glukhota <sergey@glukhota.com>
2026-02-12 21:17:06 +0100

    feat: Modify `caseSelector.py` and update Python virtual environment packages.

--- sdks/python/hyperspace/proto/hyperspace_pb2_grpc.py
+++ sdks/python/hyperspace/proto/hyperspace_pb2_grpc.py
@@ -3,9 +3,9 @@
 import grpc
 import warnings
 
-import hyperspace_pb2 as hyperspace__pb2
+from . import hyperspace_pb2 as hyperspace__pb2
 
-GRPC_GENERATED_VERSION = '1.78.0'
+GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
